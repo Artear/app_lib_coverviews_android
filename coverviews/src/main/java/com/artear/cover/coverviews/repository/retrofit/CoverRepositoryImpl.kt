ee6582535dfaf5f49d495d66ef8f8f2e52bb70d5
daver1419 <daver1419@gmail.com>
2019-03-27 18:15:49 -0300

    Change: clean up repo implementation

--- coverviews/src/main/java/com/artear/cover/coverviews/repository/retrofit/CoverRepositoryImpl.kt
+++ coverviews/src/main/java/com/artear/cover/coverviews/repository/retrofit/CoverRepositoryImpl.kt
@@ -5,18 +5,15 @@ import com.artear.networking.contract.Networking
 import com.artear.networking.model.retrofit.executeWith
 import retrofit2.Retrofit
 
-class CoverRepositoryImpl(
-        private val retrofit: Retrofit,
-        private val coverEndpoint: String,
-        private val networking: Networking
-) : CoverRepository {
+class CoverRepositoryImpl(private val retrofit: Retrofit,
+                          private val coverEndpoint: String,
+                          private val networking: Networking) : CoverRepository {
 
-    private val dynamicEndpoint by lazy { "${retrofit.baseUrl()}$coverEndpoint" }
+    private val dynamicEndpoint by lazy { retrofit.baseUrl().toString() + coverEndpoint }
     private val coverApi by lazy { retrofit.create(ApiCover::class.java) }
 
     override fun cover() = executeWith(networking) {
         coverApi.getCover(dynamicEndpoint)
     }
-
 
 }
