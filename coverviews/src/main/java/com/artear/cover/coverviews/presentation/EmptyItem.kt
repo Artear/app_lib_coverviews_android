c1e6c57555800f5740f1a055903d26311dc813be
daver1419 <daver1419@gmail.com>
2019-04-10 20:00:44 -0300

    Remove: Empty item

--- coverviews/src/main/java/com/artear/cover/coverviews/presentation/EmptyItem.kt
+++
@@ -1,8 +0,0 @@
-package com.artear.cover.coverviews.presentation
-
-import com.artear.cover.coveritem.presentation.model.ArtearObject
-import com.artear.cover.coveritem.presentation.model.ArtearStyle
-
-class EmptyItem : ArtearObject<NoStyle>()
-
-class NoStyle : ArtearStyle()
